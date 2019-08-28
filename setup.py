651ae8ecb63d30155652b3f6cf7e4d9a884d5d73
John Lamb <lamb.john.n@gmail.com>
2019-08-28 14:16:33 +0200

    zip_safe=False

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
         url="https://github.com/johnlamb/pyconfold",
         packages=setuptools.find_packages(),
         include_package_data=True,
+        zip_safe=False,
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: GPLv3",
