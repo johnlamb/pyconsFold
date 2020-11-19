dd9d604dd0b756e45ff029c0f9254027afb97391
John Lamb <lamb.john.n@gmail.com>
2020-11-19 11:51:53 +0100

    Write multiple contacts file, <20000 in each

--- pyconsFold/_pyconsFold_helpers.py
+++ pyconsFold/_pyconsFold_helpers.py
@@ -606,10 +606,12 @@ def rr2tbl(rr_file, tbl_file, rrtype, dir_out, dist):
                         "{:.2f} {:.2f} ".format(distance, negdev) +
                         "{:.2f}".format(posdev))
     # to_print.append("END")
-    if len(to_print) > 20000:
-        for i in range(1,(len(to_print)//20000) + 2):
-            print2file(tbl_file.format(i), '\n'.join(to_print[(i-1)*20000:i*20000]) + '\n')
-    print2file(tbl_file.format(1), '\n'.join(to_print) + '\n')
+    if len(to_print) > 19500:
+        for i in range(1,(len(to_print)//19500) + 2):
+            tbl_file.format(i)
+            print2file(tbl_file.format(i), '\n'.join(to_print[(i-1)*19500:i*19500]) + '\n')
+    else:
+        print2file(tbl_file.format(1), '\n'.join(to_print) + '\n')
 
 
 def contact_restraints(stage, selectrr, rrtype, dir_out, dist, rr_file=None):
