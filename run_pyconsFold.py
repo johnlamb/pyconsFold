d93898a2a749bb16609d870632a9cfc9f7d62131
John Lamb <lamb.john.n@gmail.com>
2020-11-19 14:50:22 +0100

    Prepping for benchmark running

--- run_pyconsFold.py
+++ run_pyconsFold.py
@@ -11,11 +11,12 @@ parser.add_argument("out_dir", type=str, help="Output directory to write results
 
 parser.add_argument("-fa2_file", "--fa2", type=str, help="input sequence file in FASTA format for the second sequence")
 parser.add_argument("-ss_file", "--ss", default='', type=str, help="Secondary structure file in ss2/3 format")
+parser.add_argument("-L", "--L", default=0.55 type=float, help="Cutoff for contacts")
 
 args = parser.parse_args()
 
 ########## Only uncomment ONE of these lines
-pyconsFold.model(args.fa_file, args.out_dir, rr=args.rr_file, ss=args.ss,save_step=True, tmscore_pdb_file="tests/data/2hiuA.pdb")
+pyconsFold.model(args.fa_file, args.out_dir, rr=args.rr_file, ss=args.ss,save_step=True, rr_pthres=args.L, rr_sep=0)
 # pyconsFold.model_dist(args.fa_file, args.out_dir, rr=args.rr_file)
 # pyconsFold.model_dock(args.fa_file, args.fa2_file, args.out_dir, rr=args.rr_file)
 
