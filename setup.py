02ada41cff7d171ce53753c24a203f48da51cdad
John Lamb <lamb.john.n@gmail.com>
2020-12-02 09:39:37 +0100

    setup package dir

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
         url="https://github.com/johnlamb/pyconsFold",
         packages=setuptools.find_packages(),
         include_package_data=True,
+        package_data={'': ['pyconsFold/templates/*','pyconsFold/dssp/*','pyconsFold/TMscore/*','pyconsFold/pcons/*']},
         zip_safe=False,
         classifiers=[
             "Programming Language :: Python :: 3",
